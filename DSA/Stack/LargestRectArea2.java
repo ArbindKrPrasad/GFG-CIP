942ef8396b2c79f207bf4863ba7d76fcf8702aef
ArbindKrPrasad <arbind.kr25801@gmail.com>
2021-05-08 16:00:26 +0530

    Commit

--- DSA/Stack/LargestRectArea2.java
+++ DSA/Stack/LargestRectArea2.java
@@ -1,21 +1,22 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Stack;
 
-public class LargestRectArea2 {
-    // public static void main(String[] args) {
-    //     ArrayList<Integer> arr = new ArrayList<>(Arrays.asList(2,1,5,6,2,3));
-    //     int res = largestRectArea(arr);
-    //     System.out.println(res);
-    // } 
+import javax.sql.rowset.spi.SyncFactory;
 
-    public int largestRectArea(ArrayList<Integer> arr){
-        Stack<Integer> s = new Stack<>();
+public class LargestRectArea2{
+    public static void main(String[] args) {
+        int[] arr = {2, 1, 5, 6, 2, 3};
+        
+        System.out.println(largestRectArea2(arr));
+    }
+
+    public static  int largestRectArea2(int[] arr){
         int res = 0;
-        for(int i=0; i<arr.size(); i++){
-            while(!s.empty() && arr.get(s.peek())>=arr.get(i)){
+        Stack<Integer> s = new Stack<>();
+
+        for(int i=0; i<arr.length; i++){
+            while(s.empty()==false && arr[s.peek()]>=arr[i]){
                 int top = s.pop();
-                int curr = arr.get(top) * (s.empty()?i:(i-s.peek()-1));
+                int curr = arr[top] * (s.empty()?i:i-s.peek()-1);
                 res = Math.max(res, curr);
             }
             s.push(i);
@@ -23,10 +24,10 @@ public class LargestRectArea2 {
 
         while(s.empty()==false){
             int top = s.pop();
-            int n = arr.size();
-            int curr = arr.get(top) * (s.empty()?n:(n-s.peek()-1));
+            int curr = arr[top] * (s.empty()?arr.length:arr.length-s.peek()-1);
             res = Math.max(res, curr);
         }
         return res;
-    } 
+    }
+    
 }
--- DSA/Stack/LargestRectWith1.java
+++ DSA/Stack/LargestRectWith1.java
@@ -1,7 +1,29 @@
-import LargestRectArea2;
+import org.w3c.dom.ls.LSException;
 
-public class LargestRectWith1 {
+public class LargestRectWith1 extends LargestRectArea2{
     public static void main(String[] args) {
+        int [][] arr = {{0, 1, 1, 0},
+                        {1, 1, 1, 1},
+                        {1, 1, 1, 1},
+                        {1, 1, 0, 0}};
+        int res = 0;
+        for(int i=0; i<arr.length; i++){
+            if(i==0){
+                int curr = largestRectArea2(arr[0]);
+                res = Math.max(res, curr);
+            }
+            else{
+                for(int j=0; j<arr[0].length; j++){
+                    if(arr[i][j]!=0){
+                        arr[i][j] +=arr[i-1][j];
+                    }
+                }
+                int curr = largestRectArea2(arr[i]);
+                res = Math.max(res, curr);
+            }
+        }
+
+        System.out.println(res);
         
     }
 }
